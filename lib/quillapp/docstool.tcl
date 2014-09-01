3f590628caa310cf24611847454ae773f91a4052
Will Duquette <will@wjduquette.com>
2014-09-01 12:45:16 -0700

    Updated 'quill docs' to handle quilldoc(5) docs
    'quill doc' can now format all documents of both kinds, all documents
    in a particular directory, or a particular quilldoc(5) file in the doc
    tree.

--- lib/quillapp/docstool.tcl
+++ lib/quillapp/docstool.tcl
@@ -20,17 +20,29 @@
 set ::quillapp::tools(docs) {
     command     "docs"
     description "Formats project documentation."
-    argspec     {0 1 "?<mandir>?"}
+    argspec     {0 1 "?<target>?"}
     intree      true
     ensemble    ::quillapp::docstool
 }
 
 set ::quillapp::help(docs) {
-    The "quill docs" tool formats the project's manpage(5) documentation.
+    The "quill docs" tool formats the project's quilldoc(5) and 
+    manpage(5) documentation.
 
-    By default, the tool formats all man pages.  If <mandir> is given,
-    it needs to be the name of one of the man page directories 
-    under <root>/docs, e.g., "mann".
+    By default, the tool formats all *.quilldoc documents found in 
+    <root>/docs/ and it subdirectories, and all *.manpage documents
+    in <root>/man*/.  If <target> is given, it may be one of the following:
+
+    * A man page directory name, e.g., "mann", to format all man pages
+      in the directory.
+
+    * "docs", to format all *.quilldoc documents found in <root>/docs.
+
+    * The name of a subdirectory of "docs", e.g., "mysubdir", to format
+      all *.quilldoc documents found in <root>/docs/mysubdir.
+
+    * The name of a quilldoc(5) file, relative to <root>/docs, e.g.,
+      "ug.quilldoc"; formats <root>/docs/ug.quilldoc.
 
     Quill supports four man page sections out of the box:
 
@@ -62,11 +74,187 @@ snit::type ::quillapp::docstool {
     # argv - command line arguments for this tool
     # 
     # Executes the tool given the arguments.
-    #
-    # TODO: Some of this code needs to be available to 
-    # buildtool when buildtool exists.
 
     typemethod execute {argv} {
+        # FIRST, determine the target.
+        set target [lshift argv]
+
+        # If no target is given, format all.
+        if {$target eq ""} {
+            FormatAllQuillDocs
+            FormatAllManPages 
+            return
+        }
+
+        # If it's a file, format it as a quilldoc(5) document.
+        set filename [project root docs $target] 
+
+        if {[file isfile $filename]} {
+            FormatQuillDocs [list $filename]
+            return
+        }
+
+        # If it's a man page directory, format the man pages in it.
+        if {[string match "man*" $target]} {
+            set mandir [project root docs $target]
+            if {![file isdirectory $mandir]} {
+                throw FATAL "There is no man page directory called \"$target\"".
+            }
+
+            FormatManDirs [list $mandir]
+            return
+        }
+
+        # If it's "docs" format all quilldoc(5) files in docs/.
+        if {$target eq "docs"} {
+            FormatQuillDocDir [project root docs]
+            return
+        }
+
+        # If it's a subdirectory of docs, format all quilldoc(5) files
+        # in it.
+        set dir [project root docs $target]
+        if {[file isdirectory $dir]} {
+            FormatQuillDocDir $dir
+            return
+        }
+
+        throw FATAL "Unrecognized document target: \"$target\""
+    }
+
+    #---------------------------------------------------------------------
+    # Quill Docs
+
+    # FormatAllQuillDocs
+    #
+    # Formats all Quill documents found in docs/ and its subdirectories.
+
+    proc FormatAllQuillDocs {} {
+        set docdir docs 
+
+        set doclist [GetQuillDocs docs]
+
+        if {[got $doclist]} {
+            FormatQuillDocs $doclist
+        }
+    }
+
+    # GetQuillDocs dir
+    #
+    # dir   - A directory relative to the project root
+    #
+    # Finds all *.quilldoc files in the directory and its subdirectories.
+
+    proc GetQuillDocs {dir} {
+        # FIRST, get the quilldoc(5) files in this directory.
+        set doclist [project globfiles $dir *.quilldoc]
+
+        # NEXT, look in subdirectories
+        foreach subdir [project globdirs $dir/*] {
+            lappend doclist {*}[GetQuillDocs $subdir]
+        }
+
+        return $doclist
+    }
+
+    # FormatQuillDocDir dirname
+    #
+    # Formats all Quill documents found in the directory.
+
+    proc FormatQuillDocDir {dirname} {
+        set doclist [glob -nocomplain [file join $dirname *.quilldoc]]
+        if {[got $doclist]} {
+            FormatQuillDocs $doclist
+        } else {
+            puts "Warning: no documents to format in $dirname"
+        }
+    }
+
+    # FormatQuillDocs doclist
+    #
+    # doclist - List of full paths to the documents to format.
+    #
+    # Formats the named quilldoc(5) documents in place.
+
+    proc FormatQuillDocs {doclist} {
+        set qd [quilldoc %AUTO]
+
+        foreach doc $doclist {
+            puts "Writing [file rootname $doc].html"
+
+            $qd format $doc \
+                -header "[project name] [project version] -- [project description]" \
+                -version [project version] \
+                -manroot [GetManRoot $doc]
+        }
+    }
+
+    # GetManRoot docfile
+    #
+    # Gets the relative path from this doc file to the docs/ directory.
+
+    proc GetManRoot {docfile} {
+        set docdir [file normalize [file dirname $docfile]]
+        set docroot [project root docs]
+
+        if {$docdir eq $docroot} {
+            return "."
+        }
+
+        if {![string match "$docroot/*" $docdir} {
+            throw FATAL "Document file \"$docfile\" is not in the project document tree."
+        }
+
+        set manroot [list]
+        while {$docdir ne $docroot} {
+            lappend manroot ".."
+            set docdir [file dirname $docdir] 
+        }
+
+        return [file join $manroot]
+    }
+
+
+    #---------------------------------------------------------------------
+    # Man Pages
+
+    # FormatAllManPages
+    #
+    # Formats all man pages in all man page directories (if any)
+
+    proc FormatAllManPages {} {
+        set dirnames [project globdirs docs man*]
+
+        if {[got $dirnames]} {
+            FormatManDirs $dirnames
+        }
+    }
+
+    # FormatManDirs dirlist
+    #
+    # dirlist   - A list of full pathts to thedirectories to format.
+    #
+    # Formats all man pages in the directories.
+
+    proc FormatManDirs {dirlist} {
+        set mp [manpage %AUTO%]
+
+        foreach dir $dirlist {
+            try {
+                $mp format $dir \
+                    -header  [project header]  \
+                    -version [project version]
+            } trap SYNTAX {result} {
+                throw FATAL $result
+            } on error {result eopts} {
+                # Rethrow other errors
+                return {*}$eopts $result
+            }
+        }
+    }
+
+    proc dummy {} {
+
         # FIRST, get the directories to process.
         if {[llength $argv] > 0} {
             set mandir [lindex $argv 0]
